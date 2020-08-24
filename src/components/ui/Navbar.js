f65437055a1b8f3f94f22cab8e9875aaf2274627
Leonardo Omaña <leotangram@gmail.com>
2020-08-24 09:28:23 -0500

    Logout del usuario

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useHistory } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
-const Navbar = props => {
+const Navbar = () => {
+  const history = useHistory()
+
   const {
-    user: { name }
+    user: { name },
+    dispatch
   } = useContext(AuthContext)
+
+  const handleLogout = () => {
+    dispatch({
+      type: types.logout
+    })
+    history.replace('/login')
+  }
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -46,20 +56,13 @@ const Navbar = props => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-info">{name}</span>
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
-          >
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
   )
 }
-
-Navbar.propTypes = {}
 
 export default Navbar
