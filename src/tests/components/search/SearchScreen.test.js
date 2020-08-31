38b4471b05b4ff4e2f317f53fb140587db391f39
Leonardo Omaña <leotangram@gmail.com>
2020-08-31 16:30:55 -0500

    Pruebas del componente SearchScreen

--- src/tests/components/search/SearchScreen.test.js
+++ src/tests/components/search/SearchScreen.test.js
@@ -25,4 +25,44 @@ describe('<SearchScreen />', () => {
     expect(wrapper.find('input').prop('value')).toBe('batman')
     expect(wrapper).toMatchSnapshot()
   })
+
+  test('should displayed error if not exists the hero', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman123']}>
+        <Route path="/search" component={SearchScreen} />
+      </MemoryRouter>
+    )
+    expect(wrapper.find('.alert-danger').text().trim()).toBe(
+      'There is not a hero with batman123'
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('should called history.push()', () => {
+    const history = {
+      push: jest.fn()
+    }
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman123']}>
+        <Route
+          path="/search"
+          component={() => <SearchScreen history={history} />}
+        />
+      </MemoryRouter>
+    )
+
+    wrapper.find('input').simulate('change', {
+      target: {
+        name: 'searchText',
+        value: 'batman'
+      }
+    })
+
+    wrapper.find('form').prop('onSubmit')({
+      preventDefault() {}
+    })
+
+    expect(history.push).toHaveBeenCalledWith('?q=batman')
+  })
 })
